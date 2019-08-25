ab15e2ad09ec8a57f202ca4b7f937b75f2f79894
Matt Wilson <matt@firstflightagency.com>
2019-08-25 18:02:02 -0400

    Initial setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+//How The Game Works
+//4 crystals with random numbers between 1-12 assigned
+//Random number between 19-120 to try to match with crystals
+//4 crystals will be displayed as buttons, so will need click functions
+//need to add random amount from crystal clicked to "score" that's trying to match random number at top
+//crystal amounts remain hidden, player will know value when it's added to "score"
+//player loses if score goes above random number
+//player wins if score matches random number
+//game restarts at win or loss
+//new random number at top & with each crystal on restart
+//show number of wins & losses, don't refresh page to restart game
+
+//what I'll need
+
+//var for random number
+
+//var for each crystal? or one var that will update all?
+
+//var for wins
+
+//var for losses
+
+//var for score
+
+//click functions for each crystal
+
+//function to increase score as crystals are clicked
+
+//function to determine win or loss, if loop
+
+//functions to post wins & losses
