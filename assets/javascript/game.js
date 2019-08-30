17da78a4337144e3c0d0c2d9acfa1c442d2adef0
Matt Wilson <matt@firstflightagency.com>
2019-08-30 12:28:27 -0400

    started working on functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,28 +10,48 @@
 //new random number at top & with each crystal on restart
 //show number of wins & losses, don't refresh page to restart game
 
+$(function(){
+    // Short Document Ready
+ });
+
 //what I'll need
 
 //var for random number
 var randomNum = Math.floor(Math.random() * 102) + 19;
-console.log(randomNum);
+//console.log(randomNum);
 //var for each crystal? or one var that will update all?
 var crystalOneNum = Math.floor(Math.random() * 13);
-console.log("C 1: " + crystalOneNum);
+//console.log("C 1: " + crystalOneNum);
 var crystalTwoNum = Math.floor(Math.random() * 13);
-console.log("c 2: " + crystalTwoNum);
+//console.log("c 2: " + crystalTwoNum);
 var crystalThreeNum = Math.floor(Math.random() * 13);
-console.log("c 3: " + crystalThreeNum);
+//console.log("c 3: " + crystalThreeNum);
 var crystalFourNum = Math.floor(Math.random() * 13);
-console.log("c 4: " + crystalFourNum);
+//console.log("c 4: " + crystalFourNum);
 //var for wins
 var wins = 0;
 //var for losses
 var losses = 0;
 //var for score
 var score = 0;
+//function to reset/ start game
+$(function() {
+    score=0;
+})
 //click functions for each crystal
-
+$("#crystal1").on("click", function() {
+    score + crystalOneNum;
+    console.log(score);
+})
+$("#crystal2").on("click", function() {
+    score + crystalTwoNum;
+})
+$("#crystal3").on("click", function() {
+    score + crystalThreeNum;
+})
+$("#crystal4").on("click", function() {
+    score + crystalFourNum;
+})
 //function to increase score as crystals are clicked
 
 //function to determine win or loss, if loop
