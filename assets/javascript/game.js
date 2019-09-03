419dda4a55f96a09a76f49841a1a0c2563c57fc2
Matt Wilson <matt@firstflightagency.com>
2019-09-03 11:33:43 -0400

    Further work on functions, tyring to debug click functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,24 +10,24 @@
 //new random number at top & with each crystal on restart
 //show number of wins & losses, don't refresh page to restart game
 
-$(function(){
-    // Short Document Ready
- });
+//$(function(){
+// Short Document Ready
+//});
 
 //what I'll need
 
 //var for random number
 var randomNum = Math.floor(Math.random() * 102) + 19;
-//console.log(randomNum);
+console.log(randomNum);
 //var for each crystal? or one var that will update all?
 var crystalOneNum = Math.floor(Math.random() * 13);
-//console.log("C 1: " + crystalOneNum);
+console.log("C 1: " + crystalOneNum);
 var crystalTwoNum = Math.floor(Math.random() * 13);
-//console.log("c 2: " + crystalTwoNum);
+console.log("c 2: " + crystalTwoNum);
 var crystalThreeNum = Math.floor(Math.random() * 13);
-//console.log("c 3: " + crystalThreeNum);
+console.log("c 3: " + crystalThreeNum);
 var crystalFourNum = Math.floor(Math.random() * 13);
-//console.log("c 4: " + crystalFourNum);
+console.log("c 4: " + crystalFourNum);
 //var for wins
 var wins = 0;
 //var for losses
@@ -35,22 +35,36 @@ var losses = 0;
 //var for score
 var score = 0;
 //function to reset/ start game
-$(function() {
-    score=0;
-})
+function reset() {
+    $(function () {
+        score = 0;
+        wins = 0;
+        losses = 0;
+    })
+}
+
+$("#numberToMatch").html(randomNum);
+
 //click functions for each crystal
-$("#crystal1").on("click", function() {
+$("#crystal1").on("click", function () {
     score + crystalOneNum;
     console.log(score);
+    $("#totalScore").html(score);
 })
-$("#crystal2").on("click", function() {
+$("#crystal2").on("click", function () {
     score + crystalTwoNum;
+    $("#totalScore").html(score);
+
 })
-$("#crystal3").on("click", function() {
+$("#crystal3").on("click", function () {
     score + crystalThreeNum;
+    $("#totalScore").html(score);
+
 })
-$("#crystal4").on("click", function() {
+$("#crystal4").on("click", function () {
     score + crystalFourNum;
+    $("#totalScore").html(score);
+
 })
 //function to increase score as crystals are clicked
 
